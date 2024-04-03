1f5c3f224564b89931c250157be2163669b47bae
TheDarkTiger <thedarktiger@hotmail.com>
2024-04-03 14:38:50 +0200

    Initial commit

---
+++ MeteoPixel.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python
+#! coding: utf-8
+#! python3
+# TheDarkTiger 2024
+# Generate sounds to make a weather bulletin from text files
+
+import os
+import subprocess
+
+
+#==============================================================================
+# Eye candy
+
+def info( text ) :
+	print( "   info   : " + text )
+
+def good( text ) :
+	print( "   good   : " + text )
+
+def warning( text ) :
+	print( "(warning) : " + text )
+
+def error( text ) :
+	print( "[ ERROR ] : " + text )
+
+def draw_spacer( char="=" ) :
+	print( char*79 )
+
+
+#----------------------------------------------------------------------
+# Create dir if nonexistant (no way!, really?)
+def create_dir_if_nonexistant( folderPath ):
+	if not os.path.exists( folderPath ) :
+		os.mkdir( folderPath )
+	
+
+#----------------------------------------------------------------------
+# Generate audio from text
+# 
+# default is espeak, British English
+def generate_speech( text, file ):
+	
+	cmd = f'espeak -p 60 -s 150 -v mb\mb-en1 -w "{file}" "{text}"'
+	info( f"Generate speech: {text}" )
+	subprocess.call( cmd )
+	
+
+
+#----------------------------------------------------------------------
+# Normalize sound
+# 
+# default is 44100Hz, 16bits, mono
+def normalize_sound( fileIn, fileOut, volume=1 ):
+	
+	if os.path.exists( fileIn ):
+		cmd = f'sox -v {volume} "{fileIn}" -r 44100 -b 16 -c 1 "{fileOut}"'
+		info( f"Normalize file: {fileIn}" )
+		subprocess.call( cmd )
+	else:
+		error( f"Can't normalize {fileIn}, file don't exist" )
+	
+
+
+#----------------------------------------------------------------------
+# Load a bulletin project file
+# 
+# Simple text file for now, will probably change in the future
+def bulletin_load( fileName ):
+	
+	lines = []
+	if os.path.exists( fileName ) :
+		with open( fileName, "r" ) as file :
+			lines = file.readlines()
+	else :
+		error( "File {fileName} don't exist" )
+	
+	return lines
+
+
+#----------------------------------------------------------------------
+# Generate the bulletin
+def bulletin_generate( bulletin, path ):
+	
+	# Make sure path exists
+	create_dir_if_nonexistant( path )
+	create_dir_if_nonexistant( f"{path}\\speech" )
+	
+	# Read each lines of the bulletin
+	# Generate speech when needed
+	index = 0
+	fileList = []
+	for line in bulletin:
+		line = line.strip()
+		if (line[0]=='[') and (line[-1]==']'):
+			rawFile = line[1:-1]
+			volume = 1
+			
+		else:
+			rawFile = f"{path}\\speech\\{index:02}_speech.wav"
+			generate_speech( line, rawFile )
+			volume = 0.9
+			
+		
+		file = f"{path}\\{index:02}.wav"
+		
+		# The normalize is necesary for sox to stich everything in the end
+		normalize_sound( rawFile, file, volume )
+		
+		# If the normalization suceeded, add the file to the stich list
+		if os.path.exists( file ):
+			fileList.append( file )
+		
+		index += 1
+	
+	# Ask sox to stitch everything together
+	cmd = f"sox " + " ".join(fileList) + f" {path}\\out.ogg"
+	info( f"Stitch files: {cmd}" )
+	subprocess.call( cmd )
+	
+	return 0
+
+
+#=============================================================================
+# Main
+
+if __name__ == "__main__" :
+	
+	fileName = "bulletin.txt"
+	
+	bulletin = bulletin_load( fileName )
+	bulletin_generate( bulletin, "generated" )
+	
---
+++ readme.md
@@ -0,0 +1,48 @@
+# Meteo Pixel
+Dirty as hay, learn python with this file at your own risks.
+TheDarkTiger 2024
+
+
+## The scrip
+Generates an audio fake weather forecast / bulletin from a text file.
+
+## How to use install?
+
+### Classic python install
+
+* Install Python >=3
+* Run "pip install -r requirements.txt"
+* Install [espeak](https://espeak.sourceforge.net/download.html), and make sure it is available in the PATH.
+* Install [sox](https://sourceforge.net/projects/sox/files/latest/download), and make sure it is available in the PATH.
+
+You should be able to run the script "MeteoPixel.py"
+
+### Windows executable
+Not yet available
+
+
+## How to use?
+
+* Edit "bulletin.txt"
+* Run the script by using "python MeteoPixel.py"
+
+The script will stitch each lines one after the other to generate the final audio in the generated folder as "out.ogg".
+As an example, [bulletin.txt](/bulletin.txt) should generate something akind to [bulletin.ogg](/res/bulletin.ogg)
+
+By now, it can either add a sound or synthetize voice, nothing else (no efects, no mixing, etc.).
+
+
+## Tip and Tricks
+
+### bulletin format
+
+Any line staring by \[ and ending by \] indicates the audio file to add to the bulletin.
+Any other line will be send to the speech synthetizer.
+
+> [!WARNING]
+> No check as for the content of the files will be done, if you specify a broken audio file, or other kind of file, sox may not like it.
+
+
+## Future work
+No garanty of future work.
+See [TODO.txt](/TODO.txt).
